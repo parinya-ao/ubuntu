d3804ea55a0eae51b74ed18cfadd62a2361a9627
parinya-ao <flim.parinya.ao@gmail.com>
2025-02-07 17:36:39 +0700

    Remove Windows 11 theme script and add Ubuntu original theme configuration

---
+++ theme/ubuntuoriginal.fish
@@ -0,0 +1,14 @@
+gsettings set org.gnome.desktop.interface gtk-theme "Yaru-dark"
+gsettings set org.gnome.desktop.interface icon-theme "Yaru"
+gsettings set org.gnome.shell.extensions.user-theme name "Yaru-dark"
+gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/warty-final-ubuntu.png"
+gsettings set org.gnome.desktop.interface cursor-theme "Yaru"
+gsettings reset org.gnome.shell.extensions.dash-to-dock dock-position
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 36
+gsettings reset org.gnome.shell.extensions.dash-to-dock extend-height
+gsettings reset org.gnome.shell.extensions.dash-to-dock transparency-mode
+gsettings reset org.gnome.shell.extensions.dash-to-dock background-opacity
+gsettings set org.gnome.desktop.interface font-name 'Ubuntu 11'
+gsettings set org.gnome.desktop.interface document-font-name 'Ubuntu 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Ubuntu Mono 13'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Ubuntu Bold 11'
