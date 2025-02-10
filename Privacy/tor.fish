d7bbe2bd8c5a43ace866915e07966979b6053762
parinya-ao <flim.parinya.ao@gmail.com>
2025-02-10 19:09:56 +0700

    update

--- Privacy/tor.fish
+++
@@ -1,9 +0,0 @@
-# Install Tor
-sudo apt update
-sudo apt install tor -y
-
-# Configure Tor to change IP every 10 minutes
-echo "MaxCircuitDirtiness 600" | sudo tee -a /etc/tor/torrc
-
-# Restart Tor service to apply changes
-sudo systemctl restart tor
