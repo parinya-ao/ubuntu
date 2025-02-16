4bd71b70ca19eb962fcd57972aa6863d6e5a9655
parinya-ao <flim.parinya.ao@gmail.com>
2025-02-16 20:25:28 +0700

    a

--- low_level/install.fish
+++ low_level/install.fish
@@ -3,7 +3,7 @@ sudo apt-get update
 
 curl -L -o codium_1.97.2.25045_amd64.deb https://github.com/VSCodium/vscodium/releases/download/1.97.2.25045/codium_1.97.2.25045_amd64.deb
 sudo dpkg -i codium_1.97.2.25045_amd64.deb
-sudo apt-get install -f
+sudo apt-get install -f -y
 
 # compiler
 sudo apt install iverilog gtkwave -y
@@ -11,6 +11,10 @@ sudo apt install nasm -y
 sudo apt isntall spim qtspim -y
 sudo apt install gcc-riscv64-linux-gnu -y
 sudo apt install clang gcc -y
+
+# addtional 
+apt install build-essential meson gperf flex desktop-file-utils libgtk-3-dev \
+            libbz2-dev libjudy-dev libgirepository1.0-dev -y
 
 # extension vscode
 codium --install-extension ms-vscode.cpptools
