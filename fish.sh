d8934104552d0d4ee1770414b0c75df376200da6
Parinya_Aobaun <159911463+parinya-ao@users.noreply.github.com>
2025-05-17 09:51:56 +0700

    Update fish.sh

--- fish.sh
+++ fish.sh
@@ -1,6 +1,7 @@
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
 sudo apt install fish -y
 
-# Fish performance tips
 set -U fish_greeting ''             
 set -gx EDITOR nvim                 
 set -gx VISUAL nvim
@@ -8,3 +9,15 @@ set -gx PATH $HOME/.cargo/bin $PATH
 starship init fish | source        
 zoxide init fish | source
 fzf_key_bindings
+
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install jorgebucaran/fish-autosuggestions
+fisher install ilancosman/tide@v5
+fisher install jethrokuan/z
+fisher install jethrokuan/fzf
+fisher install PatrickF1/fzf.fish
+fisher install oh-my-fish/plugin-autojump
+
+
+curl -sS https://starship.rs/install.sh | sh
+starship init fish | source
