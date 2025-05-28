299c32e520bc41ea8618dbee78aefe824c905029
Parinya_Aobaun <159911463+parinya-ao@users.noreply.github.com>
2025-05-28 20:02:48 +0700

    Update cap_lock.sh

--- problem/keyboard/cap_lock.sh
+++ problem/keyboard/cap_lock.sh
@@ -1,2 +1,5 @@
 # change lagnuage to cap lock
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:none', 'grp:caps_toggle']"
+
+sudo apt install dconf-editor
+gsettings set org.gnome.desktop.input-sources xkb-options "['grp:caps_toggle']"
